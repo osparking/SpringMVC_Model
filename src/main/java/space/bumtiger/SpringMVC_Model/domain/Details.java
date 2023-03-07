4e31054a23742ea3e593381349bb2d116dd033e6
osparking <jbpark03@gmail.com>
2023-03-07 12:33:54 +0900

    details domain class created

---
+++ src/main/java/space/bumtiger/SpringMVC_Model/domain/Details.java
@@ -0,0 +1,33 @@
+package space.bumtiger.SpringMVC_Model.domain;
+
+public class Details {
+
+	private String name;
+	private String lang;
+	private String ide;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	public String getIde() {
+		return ide;
+	}
+
+	public void setIde(String ide) {
+		this.ide = ide;
+	}
+
+}
